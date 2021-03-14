cd9feb57b97392f9124c1272ad4e0172f2377e7c
Eddie Jesinsky <eddie@jesinsky.com>
2021-03-14 13:58:32 -0600

    chapter 4

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,6 +3,7 @@ require "rulers/array"
 require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Error < StandardError; end
@@ -14,16 +15,6 @@ module Rulers
       controller = klass.new(env)
       text = controller.send(act)
       [200, {'Content-Type' => 'text/html'}, [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
